58ba1e6acb105306699818cc8101f4d865efc74a
Zesust <shuuiryuu@gmail.com>
2024-05-03 20:22:05 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,8 @@
 @section('content')
     @include('default')
     <h1 style="text-align: center">Surya Mahardika</h1>
+    <h1 style="text-align: center">Surya Mahardika</h1>
+    <h1 style="text-align: center">Surya Mahardika</h1>
+    <h1 style="text-align: center">Surya Mahardika</h1>
+
 @endsection
